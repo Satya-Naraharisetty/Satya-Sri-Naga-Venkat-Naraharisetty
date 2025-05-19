2e7f2722788990aa50b76aba27e45a13ddbfe9dd
Satya Naraharisetty <venkatnaraharisetty1234@gmail.com>
2025-05-19 16:32:34 +0530

    .

--- Problem-3.py
+++ Problem-3.py
@@ -1,9 +1,12 @@
 a = int(input("Enter a: "))
 
 count = (a + 1) // 2
-series = []
-for i in range(count):
-    num = 2 * i + 1
-    series.append(str(num))
+
+# series = []
+# for i in range(count):
+#     num = 2 * i + 1
+#     series.append(str(num))
+
+series = [str(2 * i + 1) for i in range(count)]
 
 print(", ".join(series))
